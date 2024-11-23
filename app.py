c79d9facc3fbf2c94b2db240dd814c189e0a69e7
Max Hecker <maxhecker@w208-1w-v4.eduroam.dynamic.rbg.tum.de>
2024-11-23 10:08:15 +0100

    API functions

--- app.py
+++ app.py
@@ -27,17 +27,29 @@ def get_customer(customer_id):
     else:
         print(f"Error: {response.status_code}, {response.text}")
 
-# 2. Get all customers for a specific scenario
-def get_customers_for_scenario(scenario_id):
+def get_customers(scenario_id):
     url = f"{BASE_URL}/scenarios/{scenario_id}/customers"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         print(f"Error: {response.status_code}, {response.text}")
+def get_vehicles(scenario_id):
+    url = f"{BASE_URL}/scenarios/{scenario_id}/vehicles"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+def get_vehicle(vehicle_id):
+    url = f"{BASE_URL}/vehicles/{vehicle_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
 
-# Example usage
-if __name__ == "__main__":
+def get_poisson():
     num_cars = input("Number of cars\n")
     num_cust = input("Number of Customers\n")
     scenario = init_scenario(number_cars = num_cars, number_cust = num_cust)
@@ -56,3 +68,27 @@ if __name__ == "__main__":
     #plt.scatter(dfff[:,0], dfff[:,1], s = 3, color = "Orange")
     plt.show()
 
+
+def get_scenarios():
+    url = f"{BASE_URL}/scenarios"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+def metadata(scenario_id):
+    url = f"{BASE_URL}/scenario/{scenario_id}/metadata"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+
+def filter_scenario(scenario_id):
+    pass
+if __name__ == "__main__":
+    resp = get_scenarios()
+    customers = resp[0]["vehicles"]
+    #id = resp[0]['id']
+    print(len(customers))
